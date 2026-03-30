1b714ffa1ed14a14dffa77e0dcee358b7bac6ed6
laiiiii <laiiiii@foxmail.com>
2026-03-30 21:46:33 +0800

    用户注册-生成token

--- crud/users.py
+++ crud/users.py
@@ -1,7 +1,10 @@
+import uuid
+from datetime import datetime, timedelta
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.users import User
+from models.users import User, UserToken
 from schemas.users import UserRequest
 from utils import security
 
@@ -35,3 +38,23 @@ async def create_user(db: AsyncSession, user_data: UserRequest):
     await db.commit()       # 提交事务，保存到数据库
     await db.refresh(user)  # 从数据库刷新用户对象的最新数据
     return user             # 返回创建的用户对象
+
+# 生成 Token
+async def create_token(db: AsyncSession, user_id: int):
+    # 生成 Token + 设置过期时间 → 查询数据库当前用户是否有 Token → 有：更新；没有：添加
+    token = str(uuid.uuid4())
+    # 设置 Token 过期时间为 7 天后
+    expires_at = datetime.now() + timedelta(days=7)
+    query = select(UserToken).where(UserToken.user_id == user_id)
+    result = await db.execute(query)
+    user_token = result.scalar_one_or_none()
+
+    if user_token:
+        user_token.token = token
+        user_token.expires_at = expires_at
+    else:
+        user_token = UserToken(user_id=user_id, token=token, expires_at=expires_at)
+        db.add(user_token)
+        await db.commit()
+
+    return token
