114093047e4e5b89dcc66479f4f68c5676be8f1e
laiiiii <laiiiii@foxmail.com>
2026-03-31 19:44:00 +0800

    封装通用成功响应格式

--- routers/users.py
+++ routers/users.py
@@ -4,7 +4,8 @@ from starlette import status
 
 from config.db_config import get_db
 from crud import users
-from schemas.users import UserRequest
+from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse
+from utils.response import success_response
 
 router = APIRouter(prefix="/api/user", tags=["users"])
 
@@ -17,16 +18,21 @@ async def register(user_data: UserRequest, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户已存在")
     user = await users.create_user(db, user_data)
     token = await users.create_token(db, user.id)
-    return {
-      "code": 200,
-      "message": "注册成功",
-      "data": {
-        "token": token,
-        "userInfo": {
-          "id": user.id,
-          "username": user.username,
-          "bio": user.bio,
-          "avatar": user.avatar
-        }
-      }
-    }
+    # return {
+    #   "code": 200,
+    #   "message": "注册成功",
+    #   "data": {
+    #     "token": token,
+    #     "userInfo": {
+    #       "id": user.id,
+    #       "username": user.username,
+    #       "bio": user.bio,
+    #       "avatar": user.avatar
+    #     }
+    #   }
+    # }
+    # 构建认证响应数据：包含访问令牌和完整的用户信息
+    response_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
+
+    # 返回标准化的成功响应，包含注册成功消息和用户认证数据
+    return success_response(message="注册成功", data=response_data)
