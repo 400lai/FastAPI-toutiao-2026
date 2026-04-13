9a0d0ef469833a70513de8a0c3761aa344e39c30
laiiiii <laiiiii@foxmail.com>
2026-04-13 10:17:12 +0800

    收藏功能：实现删除收藏remove_favorite()

--- routers/favorite.py
+++ routers/favorite.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from config.db_config import get_db
 from crud import favorite
@@ -28,3 +29,14 @@ async def add_favorite(
 ):
     result = await favorite.add_news_favorite(db, user.username, data.news_id)
     return success_response(message="添加收藏成功", data=result)
+
+@router.delete("/remove")
+async def remove_favorite(
+        news_id: int = Query(..., alias="newsId"),
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    result = await favorite.remove_news_favorite(db, user.id, news_id)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="收藏记录不存在")
+    return success_response(message="删除收藏成功")
