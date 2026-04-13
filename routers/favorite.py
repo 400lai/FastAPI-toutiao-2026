17a2c9b7fdbd2078ed44eab797dcdb73c7de8833
laiiiii <laiiiii@foxmail.com>
2026-04-13 10:07:03 +0800

    收藏功能：实现添加收藏add_favorite()

--- routers/favorite.py
+++ routers/favorite.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from config.db_config import get_db
 from crud import favorite
 from models.users import User
-from schemas.favorite import FavoriteCheckResponse
+from schemas.favorite import FavoriteCheckResponse, FavoriteAddRequest
 from utils.auth import get_current_user
 from utils.response import success_response
 
@@ -19,3 +19,12 @@ async def check_favorite(
     is_favorite = await favorite.is_news_favorite(db, user.username, news_id)
     # Python bool → Pydantic 对象 → FastAPI 序列化 → JSON 字符串
     return success_response(message="检查收藏状态成功", data=FavoriteCheckResponse(isFavorite = is_favorite))
+
+@router.post("/add")
+async def add_favorite(
+        data: int = FavoriteAddRequest,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    result = await favorite.add_news_favorite(db, user.username, data.news_id)
+    return success_response(message="添加收藏成功", data=result)
